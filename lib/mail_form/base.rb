4dc3bb02cb1876bd40d24989fbdb85b49f3b0412
Sam Koblenski <koblenski@gmail.com>
2014-03-07 22:08:33 -0600

    add ability to clear attributes

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,5 +1,17 @@
-module Mailform
-  def self.attributes(*names)
-    attr_accessor(*names)
+module MailForm
+  class Base
+    include ActiveModel::AttributeMethods # 1) attribute methods behavior
+    attribute_method_prefix 'clear_'      # 2) clear_ is attribute prefix
+
+    def self.attributes(*names)
+      attr_accessor(*names)
+      define_attribute_methods(names)
+    end
+
+    protected
+
+      def clear_attribute(attribute)
+        send("#{attribute}=", nil)
+      end
   end
 end
